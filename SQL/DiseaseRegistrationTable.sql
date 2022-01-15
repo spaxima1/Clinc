db390675c0e0e6dfaabc90b8b4d6b0351e6200bb
spaxima1 <spaxima1@gmail.com>
2022-01-15 23:14:37 +0330

    crate DiseaseRegistration Table in sql

---
+++ SQL/DiseaseRegistrationTable.sql
@@ -0,0 +1,25 @@
+﻿use ClinicDB;
+--ثبت بیماری 
+create table DiseaseRegistration(
+	DiseaseID INT IDENTITY(1,1) PRIMARY KEY,
+	DiseaseName nvarchar(100) not null ,
+
+	--علائم بیماری
+	DiseaseSigns nvarchar(250) not null,
+	--آیدی بیمار
+	PatientID INT,
+	--آیدی پزشک
+	DoctorID INT,
+
+)
+
+alter table DiseaseRegistration add
+	CONSTRAINT FK_DiseasePatientID FOREIGN KEY (PatientID)
+	REFERENCES Patients(PatientID)
+
+
+
+alter table DiseaseRegistration add
+	CONSTRAINT FK_DiseaseDoctorID FOREIGN KEY (DoctorID)
+	REFERENCES Doctors(DoctorID)
+	
