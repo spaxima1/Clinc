618f73af5cd1aa41c02ecbe003457f9092e51ac2
spaxima1 <spaxima1@gmail.com>
2022-01-15 21:59:09 +0330

    crate Doctor Table in sql

---
+++ SQL/DoctorTable.sql
@@ -0,0 +1,11 @@
+use ClinicDB;
+CREATE TABLE Doctors(
+	DoctorID INT IDENTITY(1,1) PRIMARY KEY,
+	DoctorUserName varchar(50) not null ,
+	DoctorName nvarchar(70) not null,
+	DoctorFamily nvarchar(70) not null,
+	DoctorPassword varchar(128) not null,
+	DoctorStartDate DATE DEFAULT  GETDATE(),
+	DoctorEndDate DATE,
+	CONSTRAINT UC_Clerk UNIQUE (DoctorUserName)
+)
